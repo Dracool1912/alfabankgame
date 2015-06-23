2df13c74daae471aefdcefa7d8601b5c1b83a21f
Dracool1912 <shh.ilya@gmail.com>
2015-06-23 09:59:30 +0300

    sec com

--- 130916_mvideo_локальная_версия_2_клиента_backup/130916_mvideo_локальная_версия_2_клиента/gameClasses/PlayerAi.js
+++
@@ -1,9 +0,0 @@
-var PlayerAi = CharacterAi.extend({
-	classId: 'PlayerAi',
-
-	init: function (collisionMap, player, pathFinder) {
-        }, // end init
-        
-});
-
-if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') { module.exports = PlayerAi; }        
